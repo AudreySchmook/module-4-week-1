8f31a45b93821bef09cbdd657af9007bbf53547f
Audrey Schmook <audrey.schmook@gmail.com>
2018-06-02 10:59:01 -0500

    adding to git

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ app.get('/', function(req,res){
     res.send('Hello world');
 });
 
+//basic request
 app.get('/monkeys', function(req,res){
     res.send('We really like monkeys');
+});
+
+//wildcard request
+app.get('/monkeys/*', function(req,res){
+    res.send('Not your mothers monkey');
+});
+
+app.get('/hello/world/2', function(req,res){
+    res.send('Another hello world example');
 });
 
 app.listen(3000, function(){
